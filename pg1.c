eb8990e85ed4dc610159f28cece7af6142851585
prabapriya <sugirpraba@gmail.com>
2017-06-20 16:14:14 +0530

    Create pg1.c

---
+++ pg1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the number");
+scanf("%d", &a);
+if(a<0)
+{
+printf("A is negative")
+}
+else if(a>0)
+{
+printf("A is positive")
+}
+else
+printf("A is ZERO");
+}
