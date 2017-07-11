6fdff9eee64919faedffd457a06f1af835c13fcf
prabapriya <sugirpraba@gmail.com>
2017-07-11 17:39:23 +0530

    large.c

---
+++ large.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+void main()
+{
+int i,n,j,large=0;
+printf(“How many numbers?”);
+scanf(“%d”,&n);
+for(i=0;i<n;++i)
+{
+printf(“nEnter number %d:”,i+1);
+scanf(“%d”,&j);
+if(j>large)
+large=x;
+}
+printf(“nnThe largest number is %d”,large);
+}
