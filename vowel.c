6ee4eede402ffea047ac6d3a7df454f44ed2ce63
prabapriya <sugirpraba@gmail.com>
2017-07-11 17:33:31 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,14 +2,11 @@
 int main()
 {
     char c;
-    int isLowercaseVowel, isUppercaseVowel;
-
     printf("Enter an alphabet: ");
     scanf("%c",&c);
-    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-    if (isLowercaseVowel || isUppercaseVowel)
-        printf("%c is a vowel.", c);
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        else if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            printf("%c is a vowel.", c);
     else
         printf("%c is a consonant.", c);
     return 0;
